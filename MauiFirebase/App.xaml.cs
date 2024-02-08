7a582f35508e42e77f6a2b7a7784295e0a8921a3
Babürşah Baz <bazbabursah1221@gmail.com>
2024-02-08 15:38:48 +0300

    Add project files.

---
+++ MauiFirebase/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiFirebase
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
